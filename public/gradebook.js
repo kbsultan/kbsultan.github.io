f08c171ebf711af50f986794f6b4bdc5c70db0e9
kbsultan <kbsultan@my.waketech.edu>
2025-07-14 20:13:40 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,10 +1,33 @@
-function fetchGradeData() {
-  console.log("fetchGradeData called");
-}
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Gradebook</title>
+  <style>
+    table {
+      width: 100%;
+      border-collapse: collapse;
+    }
+    th, td {
+      border: 1px solid black;
+      padding: 8px;
+      text-align: center;
+    }
+    th {
+      background-color: #f2f2f2;
+    }
+  </style>
+</head>
+<body>
+  <h1>Student Gradebook</h1>
+  <table id="gradebook">
+    <tr>
+      <th>Student Name</th>
+      <th>Assignment 1</th>
+      <th>Assignment 2</th>
+      <th>Assignment 3</th>
+    </tr>
+  </table>
 
-function populateGradebook() {
-  console.log("populateGradebook called");
-}
-
-fetchGradeData();
-populateGradebook();
+  <script src="gradebook.js"></script>
+</body>
+</html>
